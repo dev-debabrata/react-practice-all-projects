ec72170eed14fd7f0372411c01b9cfb666fff20a
Debabrata Das <debabratadas711@gmail.com>
2025-12-16 22:15:47 +0530

    16-12-2025

--- 10-advance-redux-react/src/main.jsx
+++ 10-advance-redux-react/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import store from './store/index';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
